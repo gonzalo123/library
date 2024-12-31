695de276d5d0c0a7b670ad6237fbffdc05f51c18
Gonzalo Ayuso <gonzalo123@gmail.com>
2024-12-31 16:13:18 +0100

    wip

--- library/dist/resources/sap/m/IllustratedMessageSize.js
+++
@@ -1,7 +0,0 @@
-/*!
- * OpenUI5
- * (c) Copyright 2009-2024 SAP SE or an SAP affiliate company.
- * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
- */
-sap.ui.define(["sap/ui/base/DataType"],function(e){"use strict";var t={Auto:"Auto",Base:"Base",Dot:"Dot",Spot:"Spot",Dialog:"Dialog",Scene:"Scene"};e.registerEnum("sap.m.IllustratedMessageSize",t);return t});
-//# sourceMappingURL=IllustratedMessageSize.js.map
