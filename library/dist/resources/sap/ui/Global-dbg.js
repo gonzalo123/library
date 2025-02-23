0b9ac6ad757253d2b7206a8d490f04ac431d8299
Gonzalo Ayuso <gonzalo123@gmail.com>
2025-02-23 15:34:43 +0100

    wip

--- library/dist/resources/sap/ui/Global-dbg.js
+++ library/dist/resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.131.1",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20250223-1519" }
+		buildinfo : { lastchange : "", buildtime : "20250223-1533" }
 	};
 
 	/**
